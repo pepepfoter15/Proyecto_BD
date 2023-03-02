bf33611eb63469e02b1964666cb5559e5a4ae84c
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-03-03 00:27:11 +0100

    Add files via upload

---
+++ EjercicioOracle.py
@@ -0,0 +1,27 @@
+from FuncionesOracle import *
+
+db = conectar_BD("localhost","pepe","root","pythonbbdd")
+opcionelegida = menu()
+
+while opcionelegida != 7:
+
+    if opcionelegida == 1:
+        opcion1(db)
+    
+    elif opcionelegida == 2:
+        opcion2(db)
+
+    elif opcionelegida == 3:
+        opcion3(db)
+
+    elif opcionelegida == 4:
+        opcion4(db)
+
+    elif opcionelegida == 5:
+        opcion5(db)
+
+    elif opcionelegida == 6:
+        opcion6(db)
+
+if opcionelegida == 7:
+    desconectar_BD(db)
